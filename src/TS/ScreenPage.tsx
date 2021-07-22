43c00982020245744b977fa0c1d21331026f40f3
Twilight0341 <y07885@gmail.com>
2021-07-22 11:32:42 +0800

    version 0.3
    Here have some things change to be:
    1.  Finish the basic display UI
        1.1 Student's personal information
        1.2 Stars
        1.3 Ranking
    
    2. display UI (can change the data by state (React real time update variable)
         2.1 Student's personal information
         2.2 Stars
    
    3. Ranking dashboard can be rotated

--- src/TS/ScreenPage.tsx
+++ src/TS/ScreenPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Styles from "../CSS/ScreenPage.module.scss";
+import CSS from "csstype";
 
+import Star from "../graph/star.svg";
 import Board from "../graph/meter.svg";
 import Pin from "../graph/pin.svg";
 
@@ -11,7 +13,7 @@ interface State {
   time: number;
   result: { sore: number; totalSore: number };
   mean: { member: number; totalMember: number };
-  star: number;
+  star: number[];
   ranking: number;
 }
 
@@ -26,13 +28,25 @@ export default class Report extends React.Component<{}, State> {
       time: 48,
       result: { sore: 50, totalSore: 58 },
       mean: { member: 36, totalMember: 58 },
-      star: 5,
+      star: [1, 1, 1, 1, 1],
       ranking: 4,
     };
   }
   componentDidMount() {
     document.body.style.backgroundColor = "#f2928c";
   }
+
+  pinRotate: CSS.Properties = {
+    position: "absolute",
+    alignItems: "flex-end",
+    width: "40%",
+    height: "90%",
+    top: "45%",
+    right: "50%",
+    transform: "rotate(10deg)",
+    transformOrigin: "86% 50%",
+    transition: "100ms",
+  };
 
   render() {
     return (
@@ -43,32 +57,93 @@ export default class Report extends React.Component<{}, State> {
             <div id={Styles["title"]}>Andy's Record Card</div>
           </div>
 
+          {/* /////////////////////////////////////////////////////////////////////////// */}
+
           {/* Student's personal information */}
-          <div id={Styles["information-list"]}>
-            <div>
-              <div className={Styles["information-border"]}>
-                Name : {this.state.name}
+          <div id={Styles["upper-content-grid-wrapper"]}>
+            <div id={Styles["upper-content-grid-wrapper-left"]}>
+              <div id={Styles["information-list"]}>
+                <div>
+                  {/* Student's name */}
+                  <div className={Styles["information-border"]}>
+                    Name : {this.state.name}
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+
+                  {/* Student's paper code */}
+                  <div
+                    className={Styles["information-border"]}
+                    style={{ width: "max-content", paddingRight: "5%" }}
+                  >
+                    Paper : {this.state.paper}
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+
+                  {/* Student's testing date */}
+                  <div className={Styles["information-border"]}>
+                    Date : {this.state.date.year}-{this.state.date.month}-
+                    {this.state.date.day}
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+
+                  {/* Student's testing date */}
+                  <div className={Styles["information-border"]}>
+                    Time : {this.state.time} mins
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+
+                  {/* Student's result */}
+                  <div className={Styles["information-border"]}>
+                    Result : {this.state.result.sore} {" / "}
+                    {this.state.result.totalSore}
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+
+                  {/* Student's means */}
+                  <div className={Styles["information-border"]}>
+                    Means of class : {this.state.mean.member} {" / "}
+                    {this.state.mean.totalMember}
+                  </div>
+
+                  {/* /////////////////////////////////////////////////////////////////////////// */}
+                </div>
               </div>
-              <div
-                className={Styles["information-border"]}
-                style={{ width: "max-content" }}
-              >
-                Paper : {this.state.paper}
-              </div>
-              <div className={Styles["information-border"]}>
-                Date : {this.state.date.year}-{this.state.date.month}-
-                {this.state.date.day}
-              </div>
-              <div className={Styles["information-border"]}>
-                Time : {this.state.time} mins
-              </div>
-              <div className={Styles["information-border"]}>
-                Result : {this.state.result.sore} {" / "}
-                {this.state.result.totalSore}
-              </div>
-              <div className={Styles["information-border"]}>
-                Means of class : {this.state.mean.member} {" / "}
-                {this.state.mean.totalMember}
+            </div>
+
+            <div id={Styles["upper-content-grid-wrapper-right"]}>
+              <div id={Styles["iconic-data-list"]}>
+                <div id={Styles["iconic-data-content"]}>
+                  <div id={Styles["iconic-data-star-wrapper"]}>
+                    {this.state.star.map((value, index) => {
+                      return (
+                        <img
+                          id={Styles["iconic-data-star-icon"]}
+                          src={Star}
+                          alt="I am star"
+                        ></img>
+                      );
+                    })}
+                  </div>
+                  <div id={Styles["iconic-data-dashboard"]}>
+                    <div id={Styles["iconic-data-rank"]}>Ranking</div>
+                    <img
+                      id={Styles["iconic-data-board"]}
+                      src={Board}
+                      alt="I am Pin"
+                    />
+                    <img
+                      id={Styles["iconic-data-pin"]}
+                      style={{ transform: "rotate(30deg)" }}
+                      src={Pin}
+                      alt="I am Pin"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
